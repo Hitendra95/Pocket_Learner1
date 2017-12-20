a084d11bc4940d255a866e39c12a4d74bcaeb3ec
Hitendra Dubey <hitendradubey@hitendras-macbook-air.local>
2017-12-20 20:53:16 +0530

    Initial Commit

---
+++ Pocket Learner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pocket Learner
+//
+//  Created by Hitendra Dubey on 12/20/17.
+//  Copyright © 2017 Hitendra Dubey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
